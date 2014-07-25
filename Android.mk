f50e925c766cd92ad6eed79b8e03d8f431332774
jrior001 <jriordan001@gmail.com>
2014-07-25 09:57:26 -0400

    wrap all the things in BACON!!
    Change-Id: I2f46c9dc7e399aeb6d75a44e1c93840f3ab73109

--- Android.mk
+++ Android.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+ifeq ($(TARGET_DEVICE),bacon)
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_VENDOR),oppo)
@@ -175,5 +177,6 @@ $(shell mkdir -p $(TARGET_OUT)/etc/firmware/wlan/prima; \
     ln -sf /data/misc/wifi/WCNSS_qcom_cfg.ini \
 	    $(TARGET_OUT)/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini)
 
+endif
 endif
 endif
